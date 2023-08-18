7c6c8fa6a6db5a1a864d02c3ae59624efdfc8d77
Miles Pieterse <milesyusuf123@gmail.com>
2023-08-18 19:44:48 +0200

    Add: file that contains markup for site logo

---
+++ config/logo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="<?php echo $redirectLink ?>">
+        <img src="<?php echo $imageLink ?>" alt="site-logo">
+    </a>
+</body>
+
+</html>
