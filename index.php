840e9a1c9399846ccd4afa16b944d44112c42ba6
Miles Pieterse <milesyusuf123@gmail.com>
2023-08-26 01:18:56 +0200

    Update: changes made to markup & page layout + added outstanding hotel data instances

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <?php require_once __DIR__ . "/include/displayHotels.php"; ?>
 
 <?php
-if (isset($_SESSION["username"])) {
-    $username = $_SESSION["username"];
+if (isset($_SESSION["userID"])) {
+    $userID = $_SESSION["userID"];
 }
 ?>
 
@@ -19,87 +19,90 @@ if (isset($_SESSION["username"])) {
 </head>
 
 <body>
-    <nav>
-        <div class="logo">
-            <?php $imageLink = "./assets/images/logo.png" ?>
-            <?php include __DIR__ . "/./config/logo.php" ?>
-        </div>
-
-        <div class="navItems" id="center">
-            <a href="./index.php">Home</a>
-            <a href="./views/customer/hotel.php">Hotels</a>
-            <a href="#">Contact</a>
-        </div>
-
-        <!-- Markup if user is not signed in -->
-        <?php if (!isset($username)) : ?>
-            <div class="navItems" id="right">
-                <a href="./register.php">
-                    <p>Register</p>
-                </a>
-                <hr>
-                <a href="./login.php">
-                    <p>Login</p>
-                </a>
+        <nav>
+            <div class="logo">
+                <?php $redirectLink = "./index.php" ?>
+                <?php $imageLink = "./assets/images/logo.png" ?>
+                <?php include __DIR__ . "/./config/logo.php" ?>
             </div>
-        <?php endif; ?>
-        <!-- ... -->
 
-        <!-- Markup if user is signed in -->
-        <?php if (isset($username)) : ?>
-            <div class="navItems" id="right">
-                <!-- <hr> -->
-                <a href="./views/customer/profile.php">
-                    <img src="./assets/images/icons/userIcon.png" alt="userIcon">
-                    <p><?php echo $username; ?></p>
-                </a>
-                <hr>
+            <!-- Markup if user is not signed in -->
+            <?php if (!isset($userID)) : ?>
+                <div class="navItems" id="right">
+                    <a href="./register.php">
+                        <p>Register</p>
+                    </a>
+                    <hr>
+                    <a href="./login.php">
+                        <p>Login</p>
+                    </a>
+                </div>
+            <?php endif; ?>
+            <!-- ... -->
 
-                <a href="./include/logoutProcess.php">
-                    <p>Logout</p>
-                </a>
+            <!-- Markup if user is signed in -->
+            <?php if (isset($userID)) : ?>
+                <div class="navItems" id="right">
+                    <a href="./views/customer/profile.php">
+                        <img src="./assets/images/icons/userIcon.png" alt="userIcon">
+                    </a>
+                    <hr>
+                    <a href="./include/logoutProcess.php">
+                        <p>Logout</p>
+                    </a>
+                </div>
+            <?php endif; ?>
+            <!-- ... -->
+        </nav>
+
+        <!-- Landing -->
+        <section class="landingContainer">
+            <div class="innerContainer">
+                <div class="innerText">
+                    <h1>Elevate Your Getaways</h1>
+                    <h2>Discover, Reserve, Experience</h2>
+                    <a class="scrollBtn" href="#hotels">Find Your Perfect Stay</a>
+                </div>
             </div>
-        <?php endif; ?>
-        <!-- ... -->
-    </nav>
-
-    <!-- Landing -->
-    <section class="landingContainer">
-        <div class="innerContainer">
-            <div class="innerText">
-                <h1>Elevate Your Getaways</h1>
-                <h2>Discover, Reserve, Experience</h2>
-                <a class="scrollBtn" href="#hotels">Find Your Perfect Stay</a>
-            </div>
-        </div>
-    </section>
+        </section>
     <!-- Landing end -->
 
     <!-- Content section -->
     <section class="hotelSection" id="hotels">
         <?php foreach ($hotels as $hotel) : ?>
-            <div class="hotelContainer">
-                <div class="thumbnail">
-                    <img src="<?php echo $hotel["thumbnail"]; ?>" alt="hotel thumbnail">
-                </div>
-
-                <div class="informationContainer">
-
-                    <div class="information">
-                        <span><?php echo $hotel["name"]; ?></span>
-                        <span><?php echo $hotel["beds"]; ?></span>
-                        <span><?php echo $hotel["type"]; ?></span>
+            <?php if (isset($userID)) : ?>
+                <form action="./views/customer/hotel.php" method="post">
+                <?php endif; ?>
+                <input type="hidden" value="<?php echo $hotel["hotel_id"]; ?>" name="hotelID">
+                <button type="submit" class="hotelContainer">
+                    <div class="thumbnail">
+                        <img src="<?php echo $hotel["thumbnail"]; ?>" alt="hotel thumbnail">
                     </div>
 
-                    <div class="information">
-                        <span><?php echo $hotel["city"]; ?></span>
-                        <span>0</span>
-                        <span><?php echo "R" . $hotel["cost_per_night"]; ?></span>
+                    <div class="informationContainer">
+
+                        <div class="information">
+                            <span><?php echo $hotel["name"]; ?></span>
+                            <div class="beds">
+                                <img src="./assets/images/icons/beds.png" alt="bed icon">
+                                <span><?php echo $hotel["beds"]; ?></span>
+                            </div>
+                            <span><?php echo $hotel["type"]; ?></span>
+                        </div>
+
+                        <div class="information">
+                            <span id="city"><?php echo $hotel["city"]; ?></span>
+                            <div class="rating">
+                                <img src="./assets/images/icons/ratingStar.png" alt="rating star">
+                                <span id="rating"><?php echo $hotel["rating"] ?></span>
+                            </div>
+                            <span id="price"><?php echo "R" . $hotel["cost_per_night"]; ?></span>
+                        </div>
                     </div>
 
-                </div>
-            </div>
-        <?php endforeach; ?>
+                </button>
+                </form>
+            <?php endforeach; ?>
     </section>
     <!-- Content section end -->
 </body>
