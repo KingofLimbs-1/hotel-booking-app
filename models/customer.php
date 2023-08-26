e2211252804a257179b6e575b9799dd4459a3f33
Miles Pieterse <milesyusuf123@gmail.com>
2023-08-26 05:16:45 +0200

    Update: class with method that fetches the information of currently signed-in user + method that allows user to update their information

--- models/customer.php
+++ models/customer.php
@@ -16,16 +16,16 @@ class customer
 
     //  METHODS
 
-    // Display hotels
-    public function displayHotels()
+    // Display user information
+    public function displayLoggedInUsersInfo($userID)
     {
-        $sqlQuery = "SELECT * FROM hotels";
+        $sqlQuery = "SELECT * FROM users where user_id = ?";
         $statement = $this->conn->prepare($sqlQuery);
+        $statement->bind_param('s', $userID);
 
         if ($statement->execute()) {
-            $hotels = $statement->get_result()->fetch_all(MYSQLI_ASSOC);
-            $statement->close();
-            return $hotels;
+            $userInfo = $statement->get_result()->fetch_all(MYSQLI_ASSOC);
+            return $userInfo;
         } else {
             echo "Query failed";
             return [];
@@ -33,21 +33,34 @@ class customer
     }
     // ...
 
-    // Display selected hotel information
-    public function displaySelectedHotelInfo($hotelID)
+
+    // Edit user information
+    public function editUserInformation($userID, $property, $newValue)
     {
-        $sqlQuery = "SELECT * FROM hotels WHERE hotel_id = ?";
+        // Query to update user information based on specified property
+        switch ($property) {
+            case "username":
+                $sqlQuery = "UPDATE users SET username = ? WHERE user_id = ?";
+                break;
+            case "fullname":
+                $sqlQuery = "UPDATE users SET fullname = ? WHERE user_id = ?";
+                break;
+            case "email":
+                $sqlQuery = "UPDATE users SET email = ? WHERE user_id = ?";
+                break;
+                // 
+            default:
+                return false;
+        }
         $statement = $this->conn->prepare($sqlQuery);
-        $statement->bind_param('i', $hotelID);
+        $statement->bind_param('si', $newValue, $userID);
 
         if ($statement->execute()) {
-
-            $hotelInfo = $statement->get_result()->fetch_all(MYSQLI_ASSOC);
-            return $hotelInfo;
+            return true;
         } else {
-            echo "Query failed";
-            return [];
+            return false;
         }
     }
+    // ...
 }
 ?>
